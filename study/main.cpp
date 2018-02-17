0f6c20445ee90307f429869a1e0579cd5910b1b5
Eivydas Raulynaitis <eivydas22@gmail.com>
2018-02-17 00:19:24 -0600

    Initial Commit

---
+++ study/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  study
+//
+//  Created by Eivydas Raulynaitis on 2/17/18.
+//  Copyright © 2018 eivydas-r. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
