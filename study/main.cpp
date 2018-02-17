3f4302bf6bddb7199bafa0f03257f4f5d96e8243
eivydas-r <erauly2@uic.edu>
2018-02-17 00:50:19 -0600

    function for reading files works

--- study/main.cpp
+++ study/main.cpp
@@ -1,16 +1,35 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 using namespace std;
 
-int main(){
+void writeToFile(){
     ofstream dataFile("data.txt");
-    dataFile.open("data.txt");
+    dataFile.open("data.txt"); //stops working here
     
     if (dataFile.is_open()){
-        dataFile << "This is a test.\n";
-        dataFile << "This is also a test.\n";
+        dataFile << "This is a test." << endl;
+        dataFile << "This is also a test." << endl;
         dataFile.close();
     } else cout << "Unable to open file";
+}
+
+void readFile(string name){
+    string line;
+    ifstream dataFile(name);
+    
+    if (dataFile.is_open()){
+        while (getline(dataFile,line)){
+            cout << line << endl;
+        }
+        
+        dataFile.close();
+    } else cout << "Unable to open file";
+}
+
+
+int main(){
+    readFile("data.txt");
     
     return 0;
 }
