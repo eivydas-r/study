6a147265a7f242a61c618a81ee4b17245e4db1d3
eivydas-r <erauly2@uic.edu>
2018-03-06 22:58:23 -0600

    ADDED: main menu, TODO: stop double input because of main menu for [A] or others

--- study/main.cpp
+++ study/main.cpp
@@ -37,7 +37,7 @@ void addAnswer(bool& terminate, char letter){
     string answer;
     getline(cin,answer);
     
-    if (answer[0] == 'x'){
+    if (tolower(answer[0]) == 'x'){
         terminate = true;
     } else {
         writeToFile("data.txt", prefix + answer);
@@ -46,7 +46,7 @@ void addAnswer(bool& terminate, char letter){
 }
 
 void addQuestion(){
-    cout << "** Enter prompt:" << endl << "[?] ";
+    cout << endl << "** Enter prompt:" << endl << "[?] ";
     string prompt;
     getline(cin,prompt);
     writeToFile("data.txt", "[?] " + prompt + "\n");
@@ -93,13 +93,86 @@ void addQuestion(){
             count++;
         }
     }
-    readFile("data.txt");
+}
+
+void mainMenu(){
+    cout << "** Welcome. What would you like to do today?" << endl;
+    cout << "[A] add questions mode" << endl;
+    cout << "[B] quiz mode" << endl;
+    cout << "[C] test mode" << endl;
+    cout << "[D] read all current data" << endl;
+    cout << "[E] clear all data" << endl;
+    cout << "[Y] extra info" << endl;
+    cout << "[X] quit program" << endl;
+    cout << "-- ";
+    
+    char choice;
+    cin >> choice;
+    
+    switch (toupper(choice)) {
+        case 'A':
+            addQuestion();
+            break;
+        case 'B':
+            cout << "** Still in development." << endl;
+            break;
+        case 'C':
+            cout << "** Still in development." << endl;
+            break;
+        case 'D':
+            readFile("data.txt");
+            break;
+        case 'E':
+            cout << "** Still in development." << endl;
+            break;
+        case 'Y':
+            cout << "** Still in development." << endl;
+            break;
+        case 'X':
+            cout << "** Still in development." << endl;
+            break;
+        default:
+            cout << "** Still in development." << endl;
+            break;
+    }
 }
 
 
 int main(){
-    addQuestion();
+    mainMenu(); //inputs into [A] prompt unintentionally also
     
     
     return 0;
 }
+
+
+/*
+ ** Welcome. What would you like to do today?
+ [A] add questions mode
+ [B] quiz mode (right/wrong quizzing w/ every question
+ [C] test mode (doesn't show answers until end, gives percentage of correct)
+ [D] read all current data
+ [E] clear all data
+ [Y] extra info
+ [X] quit program
+ 
+ 
+ 
+ [Y] extra info:
+    ** Selection info
+    [A] add questions mode: allows you to add questions with answers to the data file that can be later used for other modes
+    [B] quiz mode: will quiz you on the questions in data, good for studying
+    [C] test mode: will test you on questions in data, answers and percentage correct given at the end
+    [D] read all current data: reads all current questions/answers collected in the data file
+    [E] clear all data: delete all data stored in data file permanently
+    [Y] extra info: this
+ 
+    ** Type 'X' to return to main menu:
+ 
+ 
+    ** About
+    This program was made by Eivydas Raulynaitis.
+    Feel free to check out the development history at https://github.com/eivydas-r/study
+ 
+ 
+*/
